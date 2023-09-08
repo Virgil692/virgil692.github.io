cca53037f634ca4e5774e72bb84c7c554d05fcc5
MrVirgil <83110631+Virgil692@users.noreply.github.com>
2023-09-08 13:01:43 +0800

    修复问题

--- config.js
+++ config.js
@@ -2,18 +2,12 @@ window.$docsify = {
       name: 'MrVirgil的博客',
       repo: '',
 
-      // Sidebar Configuration
-      auto2top: true,
-      loadSidebar: true,
-      maxLevel: 0,
-      subMaxLevel: 3,
       coverpage: true,
       loadNavbor: true,
 
       search: {
         placeholder: 'Type to search',
         noData: 'No matches found.',
-        // Headline depth, 1 - 6
         depth: 2,
       }
     };
